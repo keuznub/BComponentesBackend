481cfd664ebf624643c61d7b03eda68eeadd822d
Keuznub <breixopastoriza@gmail.com>
2025-02-18 12:13:53 +0100

    quitamos uso de adaptador en server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
 
 import app from "./app";
-import { libsql } from "./database/adapter";
 import {ErrorMiddleware} from "./middlewares/error.middleware"
 app.use(ErrorMiddleware)
 
-app.use(async(req,res,next)=>{
-    await libsql.sync()
-    next()
-})
+
 app.listen(process.env.PORT,()=>{
     console.log("Servidor encendido en el puerto", process.env.PORT);
     
